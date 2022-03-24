cf26f7b022b2409783ebc39757c64b34ccf827e4
Jean-Emrick <gestionjerioux@gmail.com>
2022-03-23 22:10:19 -0400

    lifetimes

--- src/http/request.rs
+++ src/http/request.rs
@@ -4,27 +4,21 @@ use std::error::Error;
 use std::fmt::{Display,Debug, Formatter, Result as FmtResult};
 use std::str::{self, Utf8Error};
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
-
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-        unimplemented!()
-    }
-}
-
-impl TryFrom<&[u8]> for Request {
+ 
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // GET /user?id=1 HTTP/1.1\r\n
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -32,14 +26,34 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
-        unimplemented!()
+
+        let mut query_string = None;
+     
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
+
+        Ok(Self {
+            path,
+            query_string,
+            method
+        })
     }
 }
 
+/* impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+ */
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     let mut iter = request.chars();
     
-    for (i, c) in request.chars().enumerate() {
+    for (i, c) in iter.enumerate() {
         if c == ' ' || c == '\r'  {
             return Some((&request[..i], &request[i + 1..]));
         }
